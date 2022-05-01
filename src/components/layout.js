0501119ddcbbb1ce2970600f0f6bcadb2850fddc
therealbeez <gabriel.agbese2@gmail.com>
2022-05-01 15:37:00 +0200

    stable

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,17 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
+import React, {useRef} from "react";
 import PropTypes from "prop-types"
+import { gsap } from "gsap";
+import { ScrollTrigger, Draggable } from "gsap/all";
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "./styles/style.css"
 import "./layout.css"
+
+gsap.registerPlugin(Draggable, ScrollTrigger);
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,30 +30,25 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <div class="container">
+        <div class="bottom-container">
+            <div class="half1"></div>
+            <div class="half2"></div>
+        </div>
+        <div class="top-container">
+            <section class="slide slide1"><p>s1</p></section>
+            <section class="slide slide2"><p>s2</p></section>
+            <section class="slide slide3"><p>s3</p></section> 
+        </div>
+
+        <div class="drag-proxy"></div>
+    </div>
     </>
   )
 }
 
+
+//end
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
