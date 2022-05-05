d60c1f5d541f00511476edea3f0a936e6b87a995
therealbeez <gabriel.agbese2@gmail.com>
2022-05-05 13:21:58 +0200

    Distinct rendered layouts for mobile and larger

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useRef,useState, useEffect} from "react";
 import PropTypes from "prop-types"
 import { gsap } from "gsap";
 import { ScrollTrigger, Draggable } from "gsap/all";
@@ -22,30 +22,67 @@ const Layout = ({ children }) => {
       }
     }
   `)
-
+    
   const scrollRef = useHorizontalScroll();
+  const [height, width] = useWindowSize();
 
-  return (
-    <>
-      <div class="container">
-        <div class="bottom-container">
-            <div class="half1"></div>
-            <div class="half2"></div>
-        </div>
-        <div ref={scrollRef} class="topcontainer">
-          <HorizontalScroll reverseScroll = {true}>
-            <section class="slide slide1"><p>s1</p></section>
-            <section class="slide slide2"><p>s2</p></section>
-            <section class="slide slide3"><p>s3</p></section>
-          </HorizontalScroll> 
-        </div>
-    </div>
-    </>
-  )
+  if(width < 720){
+    //mobile
+    return (
+      <>
+        <div className="container">
+          <div className="bottom-container">
+              <div className="half1"></div>
+              <div className="half2"></div>
+          </div>
+          <div ref={scrollRef} className="topcontainer">
+              <div className="slide slide1"><p>s1-m</p></div>
+              <div className="slide slide2">
+                <div className="half about"></div>
+                <div className="half"></div>
+              </div>
+              <div className="slide slide3"><p>s3-m</p></div>
+          </div>
+      </div>
+      </>
+    )
+  }else if(width >= 720){
+    //desktop
+    return (
+      <>
+        <div className="container">
+          <div className="bottom-container">
+              <div className="half1"></div>
+              <div className="half2"></div>
+          </div>
+          <div ref={scrollRef} className="topcontainer">
+            <HorizontalScroll reverseScroll = {true}>
+              <div className="slide slide1"><p>s1</p></div>
+              <div className="slide slide2">
+                <div className="half about"></div>
+                <div className="half"></div>
+              </div>
+              <div className="slide slide3"><p>s3</p></div>
+            </HorizontalScroll> 
+          </div>
+      </div>
+      </>
+    )}
+
+  
 }
 
-
-
+function useWindowSize() {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize([window.innerHeight, window.innerWidth]);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return size;
+}
 
 //end
 Layout.propTypes = {
